cfcbbe2f3f561e6693487d7befc85da753e4f18a
Patrik Nilsson <patrik.nilsson@kits.se>
2023-03-15 12:48:26 +0100

    gatsby

--- gatsby-types.ts
+++ gatsby-types.ts
@@ -251,6 +251,8 @@ export type DirectoryCtimeArgs = {
 export type Site = Node & {
   buildTime: Maybe<Scalars['Date']>;
   siteMetadata: Maybe<SiteSiteMetadata>;
+  port: Maybe<Scalars['Int']>;
+  host: Maybe<Scalars['String']>;
   polyfill: Maybe<Scalars['Boolean']>;
   pathPrefix: Maybe<Scalars['String']>;
   jsxRuntime: Maybe<Scalars['String']>;
@@ -1159,6 +1161,8 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime: InputMaybe<DateQueryOperatorInput>;
   siteMetadata: InputMaybe<SiteSiteMetadataFilterInput>;
+  port: InputMaybe<IntQueryOperatorInput>;
+  host: InputMaybe<StringQueryOperatorInput>;
   polyfill: InputMaybe<BooleanQueryOperatorInput>;
   pathPrefix: InputMaybe<StringQueryOperatorInput>;
   jsxRuntime: InputMaybe<StringQueryOperatorInput>;
@@ -2726,6 +2730,8 @@ export type SiteFieldsEnum =
   | 'siteMetadata___siteUrl'
   | 'siteMetadata___rssBlogTitle'
   | 'siteMetadata___rssBlogDescription'
+  | 'port'
+  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'jsxRuntime'
@@ -2861,6 +2867,8 @@ export type SiteGroupConnectionGroupArgs = {
 export type SiteFilterInput = {
   buildTime: InputMaybe<DateQueryOperatorInput>;
   siteMetadata: InputMaybe<SiteSiteMetadataFilterInput>;
+  port: InputMaybe<IntQueryOperatorInput>;
+  host: InputMaybe<StringQueryOperatorInput>;
   polyfill: InputMaybe<BooleanQueryOperatorInput>;
   pathPrefix: InputMaybe<StringQueryOperatorInput>;
   jsxRuntime: InputMaybe<StringQueryOperatorInput>;
